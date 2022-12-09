53efe21eb526ee73944ad9c0af317f3731660510
saba <sabanatarajan89@gmail.com>
2022-12-09 14:10:38 +0530

    zip file

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # echo "stopping go application"
 # ps -ef | grep goapp | grep -v grep | awk '{print $2}' | sudo xargs kill
-# kill goapp
-# exit 0
-
-# pid=`pgrep goapp`
-# if [ -n "$pid" ]
-# then
-# 	kill $pid
-# fi
-
-os. Exit()
