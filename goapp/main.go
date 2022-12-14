7f87649af0a6b84b1e4228762e73640dd6eaadfd
saba <sabanatarajan89@gmail.com>
2022-12-14 19:36:49 +0530

    zip file

--- goapp/main.go
+++ goapp/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 <<<<<<< HEAD
-		fmt.Fprintf(w, "hi %s\n", r.URL.Path)
+		fmt.Fprintf(w, "hello %s\n", r.URL.Path)
 =======
 		fmt.Fprintf(w, "amazon  %s\n", r.URL.Path)
 >>>>>>> 6e6ebf6a822b03d14a292d7350f8c4abc5487387
