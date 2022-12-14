3a27ff478b21118ee34922dad617ac5460b8551c
saba <sabanatarajan89@gmail.com>
2022-12-14 21:16:53 +0530

    zip file

--- goapp/main.go
+++ goapp/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi  %s\n", r.URL.Path)
+		fmt.Fprintf(w, "aws  %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":81", nil)
