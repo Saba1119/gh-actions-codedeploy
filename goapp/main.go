01416ed21663a61df74cbd4707257c52b1685754
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2022-12-30 16:50:13 +0530

    Update main.go

--- goapp/main.go
+++ goapp/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi pull request %s\n", r.URL.Path)
+		fmt.Fprintf(w, "hi pull request forks %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":81", nil)
