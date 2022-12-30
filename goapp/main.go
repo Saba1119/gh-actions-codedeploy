e2d7741414aacac264c84a2b32c1363fc66cdb67
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2022-12-30 14:03:26 +0530

    Update main.go

--- goapp/main.go
+++ goapp/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi webhooks %s\n", r.URL.Path)
+		fmt.Fprintf(w, "hi webhooks  .. %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":81", nil)
