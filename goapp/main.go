67c2dda595f3eb372cb0605df94713d4671f6428
Gokul-git4 <119651989+Gokul-git4@users.noreply.github.com>
2022-12-30 17:00:06 +0530

    Update main.go

--- goapp/main.go
+++ goapp/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi pull request forks %s\n", r.URL.Path)
+		fmt.Fprintf(w, "hi pull REQUEST forks %s\n", r.URL.Path)
 	})
 
 	http.ListenAndServe(":81", nil)
